5210bf306588f10838d8ff60c1b9d40c0fd2e629
Colin Jackson <colinjackson22@googlemail.com>
2018-08-06 17:30:08 +1200

    Westpac Automation Testing assessment

--- src/test/java/stepdefs/KiwiSaverCalculatorCalculations.java
+++ src/test/java/stepdefs/KiwiSaverCalculatorCalculations.java
@@ -3,31 +3,33 @@ package stepdefs;
 import controllers.KiwiSaverRetirementCalculatorController;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.apache.log4j.Category;
+import org.apache.log4j.Logger;
 import views.KiwiSaverRetirementCalculatorView;
 
 import static com.codeborne.selenide.Condition.visible;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 public class KiwiSaverCalculatorCalculations {
+
+    private static Category log = Logger.getLogger(KiwiSaverCalculatorCalculations.class);
 
     private KiwiSaverRetirementCalculatorController kiwiSaverRetirementCalculatorController = new KiwiSaverRetirementCalculatorController();
     private KiwiSaverRetirementCalculatorView kiwiSaverRetirementCalculatorView = new KiwiSaverRetirementCalculatorView();
 
     @When("^I enter calculation parameters \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\"$")
     public void iEnterCalculationParameters(String age, String employmentStatus, String currentSalary, String contribution,
-                                            String volContrib, String volContribRate, String currentBalance, String pir, String savingGoalsRequirement, String profileType){
+                                            String volContrib, String volContribRate, String currentBalance, String pir, String savingGoalsRequirement, String profileType) {
         // Would generally do some validation here, but will just push the params unchecked for the sake of the exercise
         kiwiSaverRetirementCalculatorController.calculateProjectBalance(age, employmentStatus, currentSalary, contribution,
                 volContrib, volContribRate, currentBalance, pir, savingGoalsRequirement, profileType);
         kiwiSaverRetirementCalculatorView.getProjectedBalanceFrame().shouldBe(visible.because("The Results should be visible"));
     }
 
-    @Then("^I see the project balance at retirement matches the expected value \"([^\"]*)\"$")
-    public void iSeeTheProjectBalanceAtRetirementMatchesTheExpectedValue(String expectedBalance){
+    @Then("^I see the project balance at retirement$")
+    public void iSeeTheProjectBalanceAtRetirement(){
         kiwiSaverRetirementCalculatorView.getProjectedBalanceFrame().shouldBe(visible.because("The Results should be visible"));
-        String projectedBalance = kiwiSaverRetirementCalculatorView.getProjectedBalanceResult().
-                getText().replaceAll(",", "").replace("$\n", "");
-        assertThat(String.format("The balance was $'%s', expected $'%s'", projectedBalance, expectedBalance), projectedBalance.contains(expectedBalance));
+        String projectedBalance = kiwiSaverRetirementCalculatorView.getProjectedBalanceResult().getText().replace("\n", "");
+        log.info(String.format("The search criteria provided resulted in a Projected Balance at Retirement '%s'", projectedBalance));
     }
 }
 
--- src/test/resources/features/KiwiSaverRetirementCalculator/RetirementCalculations.feature
+++ src/test/resources/features/KiwiSaverRetirementCalculator/RetirementCalculations.feature
@@ -5,10 +5,10 @@ Feature: Calculations
   Scenario Outline: KiwiSaver Retirement Calculator calculates an appropriate project balance
     Given I am on the "KiwSaver Retirement Calculator" page
     When I enter calculation parameters "<age>" "<employmentStatus>" "<currentSalary>" "<contribution>" "<volContrib>" "<volContribRate>" "<currentBalance>" "<pir>" "<savingGoalsRequirement>" "<profileType>"
-    Then I see the project balance at retirement matches the expected value "<expectedProjectedBalance>"
+    Then I see the project balance at retirement
 
     Examples:
-      | age | employmentStatus  | currentSalary | contribution | volContrib | volContribRate | currentBalance   | pir   | savingGoalsRequirement  | profileType | expectedProjectedBalance |
-      | 30  | Employed          | 82000         | 4            |            |                |                  | 17.5  |                         | high        | 313971                   |
-      | 45  | Self-employed     |               |              | 90         | Fortnightly    | 100000           | 10.5  | 290000                  | medium      | 230481                   |
-      | 55  | Not employed      |               |              | 90         | Fortnightly    | 140000           | 10.5  | 200000                  | medium      | 208574                   |
+      | age | employmentStatus  | currentSalary | contribution | volContrib | volContribRate | currentBalance   | pir   | savingGoalsRequirement  | profileType |
+      | 30  | Employed          | 82000         | 4            |            |                |                  | 17.5  |                         | high        |
+      | 45  | Self-employed     |               |              | 90         | Fortnightly    | 100000           | 10.5  | 290000                  | medium      |
+      | 55  | Not employed      |               |              | 90         | Fortnightly    | 140000           | 10.5  | 200000                  | medium      |
